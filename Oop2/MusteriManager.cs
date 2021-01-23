fd7a0121b8c68e0dab3b34002a28746f9b9d7a3e
Merve Sayar <Merve Sayar@DESKTOP-E8IJL1H>
2021-01-23 23:51:23 +0300

    Add project files.

---
+++ Oop2/MusteriManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{
+    //iş sııfları-business classes
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+
+        }
+    }
+}
